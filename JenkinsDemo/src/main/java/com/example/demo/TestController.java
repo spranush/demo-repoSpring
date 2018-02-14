06d8a2bbf4cf4c69884341a4091fa52e7937ddc1
spranush <36442506+spranush@users.noreply.github.com>
2018-02-14 12:27:20 +0530

    Add files via upload

---
+++ JenkinsDemo/src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javainuse.model.Employee;
+
+@RestController
+public class TestController {
+	
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public List<Employee> getDataFallBack() {
+    	
+    	List<Employee> empList= new ArrayList<>();
+		Employee emp = new Employee();
+		emp.setName("fallback-emp1");
+		emp.setDesignation("fallback-manager");
+		emp.setEmpId("fallback-1");
+		emp.setSalary(3000);
+		empList.add(emp);
+
+		return empList;
+		
+	}
+}
